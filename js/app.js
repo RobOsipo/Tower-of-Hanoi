be223e9f599570177ee418c7fb1cb442c9500e64
Parker Byers <parker.n.byers@gmail.com>
2021-08-23 21:05:31 -0500

    ran some tests to determine structures

--- js/app.js
+++ js/app.js
@@ -1,24 +1,53 @@
 const rodOne = []
+const rodTwo = []
+const rodThree = []
+
 let numberOfDisks = 0
 
 //create class to easily generate disks that are trackeable and manageable
 class Disk {
     constructor(priorNumberOfDisks) {
-        this.diskNumber = priorNumberOfDisks + 1;
+        //establish some keys with default values that will need to be changed by functions and relationships with outside variables (number of disks)
+        this.rank = 0;
         //create a width key that can be used as a reference for the visual width
-        //Needs to adapt to both total number of disks and its rank among them.  I couldnt think of a fluid way to do that, so i decided to create a function that refreshes the value which will default to 0
         this.width =  0;
     }
-    refreshwidth () {}
+    //set the rank according to the number of discs
+    setRank (diskRank) {
+        this.rank = diskRank
+    }
+    //set the width according to the ranking among disks
+    setWidth (diskRank) {
+        this.width = TEMPORARY
+    }
+    diskInitialization () {
+        numberOfDisks += 1
+        this.setRank(numberOfDisks)
+        this.setWidth(numberOfDisks)
+    }
+
+
+    //add the disk to the page as an element
+    conjureDisk () {
+        let thisDisk = document.createElement(`div`)
+        thisDisk.setAttribute(`class`, `disk`)
+        thisDisk.setAttribute(`id`, `disk${this.rank}`)
+        thisDisk.style.width = TEMPORARY
+        thisDisk.style.height = TEMPORARY
+        thisDisk.style.left = TEMPORARY
+        thisDisk.style.bottom = TEMPORARY
+    }
 }
 
-//CREATE A BASE OBJECT TO FIGURE out how the class should be. wasting too much time with early onset optimization or whatever
-// const testDisk = {
-//     diskNumber: 1,
-//     width = 
-// }
-
-//i guess step one would be jsut create the htmlk elememt first
+//temporary object for testing
 let testDiskElement = document.createElement(`div`)
 testDiskElement.setAttribute(`class`, `disk`)
+testDiskElement.style.width = `90%`
+testDiskElement.style.height = `10%`
+testDiskElement.style.left = `5%`
+testDiskElement.style.bottom = `0`
+testDiskElement.style.backgroundColor = `crimson`
+testDiskElement.style.position = `absolute`
+testDiskElement.style.zIndex = `9`
 document.querySelector(`#rod1-container`).appendChild(testDiskElement)
+

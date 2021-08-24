9ebecef80b38650a61cd17021424e2d3e16465bc
Parker Byers <parker.n.byers@gmail.com>
2021-08-24 15:54:05 -0500

    added borders that do not change the size of the disks

--- js/app.js
+++ js/app.js
@@ -12,7 +12,6 @@ class Disk {
         this.rank = 0;
         //create a width key that can be used as a reference for the visual width
         this.width =  0;
-        this.rod = `One`
     }
     //set the rank according to the number of discs
     setRank () {
@@ -21,7 +20,7 @@ class Disk {
     }
     //set the width according to the ranking among disks
     setWidth () {
-        //
+        //I dont want it to fully extend out, only 90% so i do it this way
         this.width = ((this.rank / numberOfDisks) * 90)
     }
     //one functione that initializes the disk and registers it with the known disk objects
@@ -30,32 +29,44 @@ class Disk {
         this.setRank()
         this.setWidth()
     }
-    //add the disk to the page as an element, in propoer starting position
+    //add the disk to the page as an element, in proper starting position
     conjureDisk () {
+        //create the document object/element
         let thisDisk = document.createElement(`div`)
+        //make it similar to all other disks
         thisDisk.setAttribute(`class`, `disk`)
+        //make it unique also
         thisDisk.setAttribute(`id`, `disk${this.rank}`)
+        //set the width for appearance
         thisDisk.style.width = `${this.width}%`
+        //set the height. I dont want it looking too tall when we dont have a lot of disks, so i set a conditional. this could backfire if someones view-port is extremely limited vertically, but i dont care right now
         if (numberOfDisks < 10) {
             thisDisk.style.height = `10%`
         }
         else {
             thisDisk.style.height = `${90 / numberOfDisks}%`
         }
-        thisDisk.style.left = `${(100 - this.width) / 2}%`
+        // thisDisk.style.left = `${(100 - this.width) / 2}%` 
+        //^^^ originally this is how i centered it before flexbox
         rodOne.push(this)
+        document.querySelector(`#disk-container-1`).appendChild(thisDisk)
     }
 }
 
 //temporary object for testing
-let testDiskElement = document.createElement(`div`)
-testDiskElement.setAttribute(`class`, `disk`)
-testDiskElement.style.width = `90%`
-testDiskElement.style.height = `10%`
-// testDiskElement.style.left = `4.5%`
-// testDiskElement.style.bottom = `0`
-// testDiskElement.style.backgroundColor = `crimson`
-// testDiskElement.style.position = `absolute`
-// testDiskElement.style.zIndex = `9`
-document.querySelector(`#disk-container-1`).appendChild(testDiskElement)
+// let testDiskElement = document.createElement(`div`)
+// testDiskElement.setAttribute(`class`, `disk`)
+// testDiskElement.style.width = `90%`
+// testDiskElement.style.height = `10%`
+// // testDiskElement.style.left = `4.5%`
+// // testDiskElement.style.bottom = `0`
+// // testDiskElement.style.backgroundColor = `crimson`
+// // testDiskElement.style.position = `absolute`
+// // testDiskElement.style.zIndex = `9`
+// document.querySelector(`#disk-container-1`).appendChild(testDiskElement)
 
+for (let i = 0; i < numberOfDisks; i++) {
+    const aDisk = new Disk()
+    aDisk.diskInitialization()
+    aDisk.conjureDisk()
+}
